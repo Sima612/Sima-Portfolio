e3c40f6ce75d842a99ad1570ea8cb67483ce7168
Sima Tran <sima101531@yahoo.com>
2023-01-07 05:47:13 -0800

    ..

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ toggleMenu();
 
 function sendEmail() {
     Email.send({
-        SecureToken: "78954b5c-0437-4778-b38c-4fc0022061d2",
-        // Host : "smtp.elasticemail.com",
-        // Username : "rachasima.tran001@gmail.com",
-        // Password : "E8B047412BE2D7B2292BD89D92622673EC07",
-        To : 'rachasima.tran001@gmail.com',
+        // SecureToken: "ba12a756-ff36-42c6-8f65-b95855e624ec",
+        Host : "smtp.elasticemail.com",
+        Username : "rachasima.tran001@gmail.com",
+        Password : "718BE7D4004469E6598683EA0DB453BCC277",
+        To : "rachasima.tran001@gmail.com",
         From : document.getElementById("email").value,
         Subject : document.getElementById("subject").value,
         Body : "Full Name: " + document.getElementById("name").value
@@ -23,7 +23,7 @@ function sendEmail() {
             + "<br> Phone #: " + document.getElementById("number").value
             + "<br> Message: " + document.getElementById("message").value
     }).then(
-      message => alert(message)
+      message => alert("Message sent successfully")
     );
 
     return;
