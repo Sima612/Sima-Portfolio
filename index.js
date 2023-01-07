8454e7fb5c4e2f4025a9a01cbccfda6f7944f667
Sima Tran <sima101531@yahoo.com>
2023-01-07 03:27:59 -0800

    adding contact functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+//Toggle menu
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+}
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "rachasima.tran001@gmail.com",
+        Password : "EBE9AD4EF5C0380D32077E33CEE3A53A000D",
+        To : 'rachasima.tran001@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
